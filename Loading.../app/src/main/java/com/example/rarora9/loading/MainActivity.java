9009b7acf2fa2422ed3f1df7ab15f0ca3244d64a
rarora9 <rarora9@yahoo.com>
2015-02-20 23:25:31 -0500

    Loading Bar works
    The loading bar now goes up and “You Won!” is displayed when it is full.

--- Loading.../app/src/main/java/com/example/rarora9/loading/MainActivity.java
+++ Loading.../app/src/main/java/com/example/rarora9/loading/MainActivity.java
@@ -7,13 +7,37 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
+
+    int width, height;
+    int loadWidth = 0, numMilliseconds = 0;
+    boolean won = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        final View view = new MyView(this);
+        setContentView(view);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(width != 0) {
+                    if ((numMilliseconds % 100) == 0 && loadWidth <= width) {
+                        loadWidth += (width / 100);
+                    } else if (loadWidth >= width) {
+                        won = true;
+                    }
+                }
+
+                view.postInvalidate();
+                numMilliseconds++;
+            }
+        }, 0, 1);
     }
 
     public class MyView extends View {
@@ -23,14 +47,24 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onDraw(Canvas canvas) {
+            width = canvas.getWidth();
+            height = canvas.getHeight();
             super.onDraw(canvas);
             Paint paint = new Paint();
             paint.setColor(Color.BLACK);
             canvas.drawPaint(paint);
             paint.setColor(Color.LTGRAY);
-            canvas.drawRect(0, 0, canvas.getWidth(), getHeight()/20, paint);
+            canvas.drawRect(0, 0, width, height/20, paint);
             paint.setColor(Color.GREEN);
-            canvas.drawRect(0, 0, canvas.getWidth()/3, getHeight()/20, paint);
+            canvas.drawRect(0, 0, loadWidth, height/20, paint);
+            paint.setColor(Color.WHITE);
+            canvas.drawCircle(width/2, height/2, width/20, paint);
+
+            if(won) {
+                paint.setTextSize(100);
+                paint.setTextAlign(Paint.Align.CENTER);
+                canvas.drawText("You Won!", width/2, height/4, paint);
+            }
         }
     }
 }
