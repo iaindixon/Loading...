ee993172333876d789eacee34203d1dd98611a95
Iain Dixon <iain@dixon.net>
2015-02-24 14:13:49 -0500

    Added Paint to Circle
    Added the Paint functionality to the circle class.

--- Loading.../app/src/main/java/com/example/rarora9/loading/Circle.java
+++ Loading.../app/src/main/java/com/example/rarora9/loading/Circle.java
@@ -1,27 +1,32 @@
 package com.example.rarora9.loading; /**
  * Created by iain on 2/20/15.
  */
-import android.graphics.Color;
+import android.graphics.Paint;
+import static android.graphics.Color.BLUE;
 
 public class Circle {
 
     private int xCoord;
     private int yCoord;
     private int radius;
-    private int color;
+    private Paint paint;
+
+    {
+        paint = new Paint();
+    }
 
     public Circle(){
         xCoord = 0;
         yCoord = 0;
         radius = 0;
-        color = Color.GREEN;
+        paint.setColor(BLUE);
     }
 
-    public Circle(int x, int y, int rad, int col){
+    public Circle(int x, int y, int rad, Paint pai){
         xCoord = x;
         yCoord = y;
         radius = rad;
-        color = col;
+        paint = pai;
     }
 
     public int getXCoord(){
@@ -36,8 +41,8 @@ public class Circle {
         return radius;
     }
 
-    public int getColor(){
-        return color;
+    public Paint getPaint(){
+        return paint;
     }
 
 
